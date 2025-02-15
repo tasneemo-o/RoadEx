2c8424d15ad388d7db3960eea4e6c3a329296c09
tasneemo-o <tasneemabdelhakeem1@gmail.com>
2025-02-16 01:58:10 +0200

    add login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,12 +1,131 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formLoginKey = GlobalKey<FormState>();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
+  void _submitForm() {
+    if (_formLoginKey.currentState!.validate()) {
+      // TODO login logic
+      Navigator.pushNamedAndRemoveUntil(context, '/home', (route)=>false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formLoginKey,
+            child: Column(
+              children: [
+                Image.asset('assets/images/intro/logo-no-slogan.png',
+                    height: 150, fit: BoxFit.cover),
+                const SizedBox(height: 16),
+                const Center(
+                  child: Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                        color: Color(0xff3A3434),
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                TextFormField(
+                  controller: usernameController,
+                  decoration: _buildInputDecoration('Username'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your password';
+                    }
+                    return null;
+                  },
+                  decoration: _buildInputDecoration('Password').copyWith(
+                    suffixIcon: IconButton(
+                      onPressed: () => setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      }),
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                      ),
+                    ),
+                  ),
+                  obscureText: !_isPasswordVisible,
+                  obscuringCharacter: '*',
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff3A3434),
+                    minimumSize: const Size(double.infinity, 60),
+                  ),
+                  child: const Text('Login',
+                      style: TextStyle(color: Colors.white, fontSize: 18)),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account? ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacementNamed(context, '/signup');
+                      },
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _buildInputDecoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle: const TextStyle(color: Colors.black54),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
     );
   }
 }
