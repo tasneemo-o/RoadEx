7e38f495a94dc9d23c054718e6af4d05ede8d80e
tasneemo-o <tasneemabdelhakeem1@gmail.com>
2025-02-15 23:41:27 +0200

    update splash screen
    - animate logo to move from bottom to center of the screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -8,42 +8,62 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool _isAnimated = false;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, '/intro');
-    });
+    startAnimation();
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xff817d7d),
-              Color(0xff3A3434),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/intro/logo.png',
-                width: 250,
-                height: 250,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff817d7d),
+                  Color(0xff3A3434),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
-            ],
+            ),
           ),
-        ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 1000),
+            bottom: _isAnimated ? screenHeight / 2 - 125 : 0,
+            right: _isAnimated ? screenWidth / 2 - 125 : screenWidth / 2 - 125,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 800),
+              opacity: _isAnimated ? 1 : 0,
+              child: Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/intro/logo.png',
+                  width: 250,
+                  height: 250,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Future startAnimation() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    setState(() {
+      _isAnimated = true;
+    });
+    await Future.delayed(const Duration(milliseconds: 1500));
+    Navigator.pushReplacementNamed(context, '/intro');
   }
 }
